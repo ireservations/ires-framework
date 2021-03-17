4fe13b864e293c688ff137be69f18ef7fd8839f1
Rudie Dirkx <github@hotblocks.nl>
2021-03-17 23:17:42 +0100

    db count_rows() bugfix

--- src/db/db_generic.php
+++ src/db/db_generic.php
@@ -257,9 +257,8 @@ abstract class db_generic {
 
 	/** @return int */
 	public function count_rows( $query ) {
-		$query = rtrim($query, ';');
-		$query = preg_replace('#SELECT[\s\S]+?FROM\s#i', 'SELECT 1 FROM ', $query, 1);
-		$count = $this->fetch_one('SELECT COUNT(1) num FROM (' . $query . ') x');
+		$query = trim(rtrim($query, ';'));
+		$count = $this->fetch_one("SELECT COUNT(1) num FROM ($query) x");
 		return $count !== false ? (int) $count : false;
 	}
 
